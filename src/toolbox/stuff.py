47dc13d4ff9c15ba2ae9558a8d7a3e0feba4c377
Julien <julienbraine@yahoo.fr>
2023-04-04 13:48:57 +0200

    stuff

---
+++ src/toolbox/stuff.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+#Based on https://stackoverflow.com/questions/3463930/how-to-round-the-minute-of-a-datetime-object
+def roundTime(dt=None, roundTo=60):
+   """Round a datetime object to any time lapse in seconds
+   dt : datetime.datetime object, default now.
+   roundTo : Closest number of seconds to round to, default 1 minute.
+   Author: Thierry Husson 2012 - Use it as you want but don't blame me.
+   """
+   if dt == None : dt = datetime.datetime.now()
+   seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+   rounding = (seconds+roundTo/2) // roundTo * roundTo
+   return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
